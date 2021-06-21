093e25b39b82d5511e0dc34a171665695deaba84
ramana24 <yellapuramana@gmail.com>
2021-06-21 03:15:56 +0000

    M table features are implemented V1

---
+++ webapp/controller/Worklist.controller.js
@@ -0,0 +1,291 @@
+sap.ui.define([
+	"./BaseController",
+	"sap/ui/model/json/JSONModel",
+	"../model/formatter",
+	"sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    'sap/ui/core/util/Export',
+    'sap/ui/core/util/ExportTypeCSV',
+    'sap/ui/export/library',
+	'sap/ui/export/Spreadsheet',
+], function (BaseController, JSONModel, formatter, Filter, FilterOperator,Export,ExportTypeCSV,exportLibrary,Spreadsheet) {
+    "use strict";
+    	var EdmType = exportLibrary.EdmType;
+
+	return BaseController.extend("ux.lorealproducts.controller.Worklist", {
+
+        formatter: formatter,
+        
+
+		/* =========================================================== */
+		/* lifecycle methods                                           */
+		/* =========================================================== */
+
+		/**
+		 * Called when the worklist controller is instantiated.
+		 * @public
+		 */
+		onInit : function () {
+			var oViewModel,
+				iOriginalBusyDelay,
+				oTable = this.byId("table");
+
+			// Put down worklist table's original value for busy indicator delay,
+			// so it can be restored later on. Busy handling on the table is
+			// taken care of by the table itself.
+			iOriginalBusyDelay = oTable.getBusyIndicatorDelay();
+			// keeps the search state
+			this._aTableSearchState = [];
+
+			// Model used to manipulate control states
+			oViewModel = new JSONModel({
+				worklistTableTitle : this.getResourceBundle().getText("worklistTableTitle"),
+				shareOnJamTitle: this.getResourceBundle().getText("worklistTitle"),
+				shareSendEmailSubject: this.getResourceBundle().getText("shareSendEmailWorklistSubject"),
+				shareSendEmailMessage: this.getResourceBundle().getText("shareSendEmailWorklistMessage", [location.href]),
+				tableNoDataText : this.getResourceBundle().getText("tableNoDataText"),
+				tableBusyDelay : 0
+			});
+			this.setModel(oViewModel, "worklistView");
+
+			// Make sure, busy indication is showing immediately so there is no
+			// break after the busy indication for loading the view's meta data is
+			// ended (see promise 'oWhenMetadataIsLoaded' in AppController)
+			oTable.attachEventOnce("updateFinished", function(){
+				// Restore original busy indicator delay for worklist's table
+				oViewModel.setProperty("/tableBusyDelay", iOriginalBusyDelay);
+			});
+        },
+        
+
+
+		/* =========================================================== */
+		/* event handlers                                              */
+		/* =========================================================== */
+
+		/**
+		 * Triggered by the table's 'updateFinished' event: after new table
+		 * data is available, this handler method updates the table counter.
+		 * This should only happen if the update was successful, which is
+		 * why this handler is attached to 'updateFinished' and not to the
+		 * table's list binding's 'dataReceived' method.
+		 * @param {sap.ui.base.Event} oEvent the update finished event
+		 * @public
+		 */
+		onUpdateFinished : function (oEvent) {
+			// update the worklist's object counter after the table update
+			var sTitle,
+				oTable = oEvent.getSource(),
+				iTotalItems = oEvent.getParameter("total");
+			// only update the counter if the length is final and
+			// the table is not empty
+			if (iTotalItems && oTable.getBinding("items").isLengthFinal()) {
+				sTitle = this.getResourceBundle().getText("worklistTableTitleCount", [iTotalItems]);
+			} else {
+				sTitle = this.getResourceBundle().getText("worklistTableTitle");
+			}
+			this.getModel("worklistView").setProperty("/worklistTableTitle", sTitle);
+		},
+
+		/**
+		 * Event handler when a table item gets pressed
+		 * @param {sap.ui.base.Event} oEvent the table selectionChange event
+		 * @public
+		 */
+		onPress : function (oEvent) {
+			// The source is the list item that got pressed
+			this._showObject(oEvent.getSource());
+		},
+
+		/**
+		 * Event handler for navigating back.
+		 * We navigate back in the browser history
+		 * @public
+		 */
+		onNavBack : function() {
+			// eslint-disable-next-line sap-no-history-manipulation
+			history.go(-1);
+		},
+
+
+		onSearch : function (oEvent) {
+			if (oEvent.getParameters().refreshButtonPressed) {
+				// Search field's 'refresh' button has been pressed.
+				// This is visible if you select any master list item.
+				// In this case no new search is triggered, we only
+				// refresh the list binding.
+				this.onRefresh();
+			} else {
+				var aTableSearchState = [];
+				var sQuery = oEvent.getParameter("query");
+
+				if (sQuery && sQuery.length > 0) {
+					aTableSearchState = [new Filter("Name", FilterOperator.Contains, sQuery)];
+				}
+				this._applySearch(aTableSearchState);
+			}
+
+		},
+
+		/**
+		 * Event handler for refresh event. Keeps filter, sort
+		 * and group settings and refreshes the list binding.
+		 * @public
+		 */
+		onRefresh : function () {
+			var oTable = this.byId("table");
+			oTable.getBinding("items").refresh();
+		},
+
+		/* =========================================================== */
+		/* internal methods                                            */
+		/* =========================================================== */
+
+		/**
+		 * Shows the selected item on the object page
+		 * On phones a additional history entry is created
+		 * @param {sap.m.ObjectListItem} oItem selected Item
+		 * @private
+		 */
+		_showObject : function (oItem) {
+			this.getRouter().navTo("object", {
+				objectId: oItem.getBindingContext().getProperty("ProductID")
+			});
+		},
+
+		/**
+		 * Internal helper method to apply both filter and search state together on the list binding
+		 * @param {sap.ui.model.Filter[]} aTableSearchState An array of filters for the search
+		 * @private
+		 */
+		_applySearch: function(aTableSearchState) {
+			var oTable = this.byId("table"),
+				oViewModel = this.getModel("worklistView");
+			oTable.getBinding("items").filter(aTableSearchState, "Application");
+			// changes the noDataText of the list in case there are no filter results
+			if (aTableSearchState.length !== 0) {
+				oViewModel.setProperty("/tableNoDataText", this.getResourceBundle().getText("worklistNoDataWithSearchText"));
+			}
+        },
+        
+        onExportCSV:function(oEvent){
+
+			var oExport = new Export({
+
+				// Type that will be used to generate the content. Own ExportType's can be created to support other formats
+				exportType : new ExportTypeCSV({
+					separatorChar : ","
+				}),
+
+				// Pass in the model created above
+				models : this.getView().getModel(),
+
+				// binding information for the rows aggregation
+				rows : {
+					path : "/ProductSet"
+				},
+
+				// column definitions with column name and binding info for the content
+
+				columns : [{
+					name : "ProductID",
+					template : {
+						content : "{ProductID}"
+					}
+                }, 
+                
+                {
+					name : "Name",
+					template : {
+						content : "{Name}"
+					}
+				},
+                
+                {
+					name : "Description",
+					template : {
+						content : "{Description}"
+					}
+				}, {
+					name : "Price",
+					template : {
+						content : "{Price}"
+					}
+				}
+				]
+			});
+
+			// download exported file
+			oExport.saveFile().catch(function(oError) {
+				MessageBox.error("Error when downloading data. Browser might not be supported!\n\n" + oError);
+			}).then(function() {
+				oExport.destroy();
+			});
+
+
+
+        },
+        onExportSpreadsheet:function(oEvent){
+
+var aCols, oRowBinding, oSettings, oSheet, oTable;
+
+			if (!this._oTable) {
+				this._oTable = this.byId('table');
+			}
+
+			oTable = this._oTable;
+			oRowBinding = oTable.getBinding('items');
+			aCols = this.createColumnConfig();
+
+			oSettings = {
+				workbook: {
+					columns: aCols,
+					hierarchyLevel: 'Level'
+				},
+				dataSource: oRowBinding,
+				fileName: 'SpreadsheetFile.xlsx',
+				worker: false // We need to disable worker because we are using a MockServer as OData Service
+			};
+
+			oSheet = new Spreadsheet(oSettings);
+			oSheet.build().finally(function() {
+				oSheet.destroy();
+			});
+        },
+
+        createColumnConfig:function(){
+            var aCols = [];
+
+			aCols.push({
+				label: 'Product Name',
+				property: 'ProductID',
+				type: EdmType.String
+				
+			});
+
+			aCols.push({
+				label: 'Category Name',
+				type: EdmType.String,
+				property: 'Category'
+			
+			});
+
+			aCols.push({
+				property: 'Price',
+                type: EdmType.Number,
+                scale:2
+			});
+
+			
+			aCols.push({
+				property: 'CreatedAt',
+				type: EdmType.Date
+			});
+
+			
+
+			return aCols;
+        },
+
+	});
+});
